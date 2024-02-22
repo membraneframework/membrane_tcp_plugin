f3bbcf548f3e48e5329a44d74e0427c1ee197d1b
noarkhh <jakub.pryc@gmail.com>
2024-02-22 14:54:44 +0100

    Add new integration test

--- test/membrane_tcp/integration_test.exs
+++ test/membrane_tcp/integration_test.exs
@@ -13,7 +13,7 @@ defmodule Membrane.TCP.IntegrationTest do
 
   @payload_frames 100
 
-  test "send and receive using 2 pipelines" do
+  test "send from server pipeline and receive on client pipeline" do
     data = Enum.map(1..@payload_frames, &"(#{&1})") ++ ["."]
 
     sender =
@@ -42,8 +42,45 @@ defmodule Membrane.TCP.IntegrationTest do
     assert_pipeline_notified(
       receiver,
       :source,
-      {:connection_info, @local_address, _ephemeral_port}
+      {:connection_info, @local_address, _port}
     )
+
+    assert_end_of_stream(sender, :sink)
+
+    received_data = TCP.TestingSinkReceiver.receive_data(receiver)
+
+    assert received_data == Enum.join(data)
+    Pipeline.terminate(sender)
+    Pipeline.terminate(receiver)
+  end
+
+  test "send from client pipeline and receive on server pipeline" do
+    data = Enum.map(1..@payload_frames, &"(#{&1})") ++ ["."]
+
+    receiver =
+      Pipeline.start_link_supervised!(
+        spec:
+          child(:source, %TCP.Source{
+            connection_side: :server,
+            local_address: @local_address,
+            local_port_no: @server_port
+          })
+          |> child(:sink, %Testing.Sink{})
+      )
+
+    sender =
+      Pipeline.start_link_supervised!(
+        spec:
+          child(:source, %Testing.Source{output: data})
+          |> child(:sink, %TCP.Sink{
+            connection_side: {:client, @local_address, @server_port},
+            local_address: @local_address
+          })
+      )
+
+    assert_pipeline_notified(sender, :sink, {:connection_info, @local_address, _port})
+
+    assert_pipeline_notified(receiver, :source, {:connection_info, @local_address, @server_port})
 
     assert_end_of_stream(sender, :sink)
 

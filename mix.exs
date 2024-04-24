e59c37bfa2c2aad0d0316a389aefed4b27a4dbb6
noarkhh <jakub.pryc@gmail.com>
2024-04-24 12:15:45 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.TCP.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @github_url "https://github.com/membraneframework/membrane_tcp_plugin"
 
   def project do
